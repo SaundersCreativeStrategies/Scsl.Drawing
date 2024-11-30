dc9651d474f876acd93c1b3d600b5a61bf847a62
Shane Saunders <asparatu@gmail.com>
2024-11-30 13:37:10 +0800

    Change name of unit test file

---
+++ tests/Scsl.Drawing.UnitTest/IFormFileUnitTests.cs
@@ -0,0 +1,12 @@
+namespace Scsl.Drawing.UnitTest;
+
+[TestClass]
+public class IFormFileUnitTests
+{
+    [TestMethod]
+    public async Task Should_Upload_Single_File()
+    {
+        // Arrange
+        
+    }
+}
